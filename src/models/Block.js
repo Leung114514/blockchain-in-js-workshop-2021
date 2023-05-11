7bfadca6ffa4783d27cabe41d2227122210c8d52
Leung114514 <2821943513@qq.com>
2023-05-11 21:13:28 +0800

    ok
    111

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,9 +1,27 @@
+import Blockchain from "./Blockchain.js";
 class Block {
+
+
   // 1. 完成构造函数及其参数
   /* 构造函数需要包含
-
+        - 区块链
+        - 前一个区块的哈希值
+        - 区块高度
+        - 区块哈希值
   */
-  constructor() {}
+  constructor(blockchain,previousHash,height,Hash) {
+    //区块链
+    this.blockchain=blockchain
+    //前哈希
+    this.previousHash=previousHash
+    //区块高度
+    this.height=height
+    //本区块哈希
+    this.Hash=Hash
+  }
+  getPreviousBlock(){
+    return this.blockchain.blocks[this.previousHash]
+  }
 }
 
 export default Block
--- src/models/Blockchain.js
+++ src/models/Blockchain.js
@@ -1,22 +1,40 @@
 // Blockchain
+
 class Blockchain {
   // 1. 完成构造函数及其参数
-  /* 构造函数需要包含 
+  /* 构造函数需要包含
       - 名字
       - 创世区块
       - 存储区块的映射
   */
-  constructor() {}
+  constructor(name) {
+    this.name = name
+    this.genensis = null
+    this.blocks = {}
+
+    if(this.genensis){
+      this.blocks[this.genensis.hash]=this.genensis
+    }
+  }
 
   // 2. 定义 longestChain 函数
-  /* 
+  /*
     返回当前链中最长的区块信息列表
   */
   longestChain() {
-
-
-    return []
+    let longestChain=[]
+    for(let hash in this.blocks){
+      let block=this.blocks[hash]
+      let chain=[block]
+      while(block.previousHash!='root'&&block.hash!='root'){
+        block=block.getPreviousBlock()
+        chain.unshift(block)
+      }
+      if(chain.length>longestChain.length){
+        longestChain=chain
+      }
+    }
+    return longestChain
   }
 }
-
 export default Blockchain
