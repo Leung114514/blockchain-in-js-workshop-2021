5f466a254d1e8d10fac08a84f9007765aeb79a89
Leung114514 <2821943513@qq.com>
2023-05-26 16:14:43 +0800

    111

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,15 +1,29 @@
-import sha256 from 'crypto-js/sha256.js'
-
-export const DIFFICULTY = 2
-
+import UTXOPool from './UTXOPool.js'
 class Block {
-  // 1. 完成构造函数及其参数
+    // 1. 完成构造函数及其参数
+    constructor(blockchain, parentHash, height, hash, miner) {
+        this.blockchain = blockchain
+        this.parentHash = parentHash
+        this.height = height
+        this.hash = hash
+        this.coinbaseBeneficiary = miner
+        this.utxoPool = new UTXOPool({})
+    }
+    getPreviousBlock () {
+        // 判断是否为高度为1的区块
+        if (this.height == 1) {
+            return this.blockchain.genesis
+        }
+        return this.blockchain.blocks[this.parentHash]
+    }
+    isValid () {
+        const leadingZero = '0'.repeat(DIFFICULTY)
 
-  constructor() {}
-
-  isValid() {}
-
-  setNonce(nonce) {}
+        return (this.nonce + "").startsWith(leadingZero)
+    }
+    setNonce (nonce) {
+        this.nonce = nonce
+    }
 }
-
 export default Block
+export const DIFFICULTY = 3
